d75d0100dada39fe2420a11cb654c91cdfd732a6
Shae Riches <shaeriches@gmail.com>
2022-11-27 17:44:00 +1100

    successful get from backend

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,22 @@
+import axios from "axios";
 import React from "react";
 import '../App.css'
 
+const MONGOOSE_BACKEND_BASE_URL = 'http://localhost:3000/projects';
+
 class Projects extends React.Component {
+
+    componentDidMount() {
+        console.log(`componentDidMount()`);
+        this.fetchProjects();
+    }
+
+    fetchProjects = async () => {
+        
+        const res = await axios.get(MONGOOSE_BACKEND_BASE_URL);
+        console.log(`response:`, res.data);
+        
+    }
 
     render() {
 
